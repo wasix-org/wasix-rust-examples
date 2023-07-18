575fdffd1f3b04fefba535b26eda2903b8e98973
Rudra <r82941037@gmail.com>
2023-07-18 15:48:24 +0100

    Add a PORT env var

--- wasix-axum/src/main.rs
+++ wasix-axum/src/main.rs
@@ -6,8 +6,14 @@ async fn main() {
     // Building our application with a single Route
     let app = Router::new().route("/", get(handler));
 
+    let port = std::env::var("PORT").unwrap_or("80".to_string());
+    let port = port.parse::<u16>().unwrap_or_else(|_| {
+        eprintln!("Invalid port number: {}", port);
+        std::process::exit(1);
+    });
     // Run the server with hyper on http://127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3002));
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    eprintln!("Listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
