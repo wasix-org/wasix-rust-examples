741290828e67b0cc79edfe2ebe1cc84254cb9423
Rudra Arora <aroraru@tcd.ie>
2023-08-28 13:52:08 +0100

    grpc example

---
+++ wasix-grpc/src/client.rs
@@ -0,0 +1,173 @@
+use std::{sync::Arc, time::Duration};
+
+use hello_world::greeter_client::GreeterClient;
+use hello_world::HelloRequest;
+
+use http_body::{combinators::UnsyncBoxBody, Body};
+use hyper::{body::Bytes, client::HttpConnector, Client, Request, Uri};
+use hyper_rustls::HttpsConnector;
+use tokio_rustls::rustls::{ClientConfig, ConfigBuilder, OwnedTrustAnchor, RootCertStore};
+use tokio_stream::{Stream, StreamExt};
+use tonic::{
+    body::BoxBody,
+    client::GrpcService,
+    transport::{Channel, ClientTlsConfig},
+    Status,
+};
+use tower::{util::MapRequest, ServiceExt};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+fn hello_requests_iter() -> impl Stream<Item = HelloRequest> {
+    tokio_stream::iter(1..usize::MAX).map(|i| HelloRequest {
+        name: format!("Hello {}", i),
+    })
+}
+
+async fn say_hello<T>(client: &mut GreeterClient<T>)
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody> + Send + 'static,
+    T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
+{
+    let request = tonic::Request::new(HelloRequest {
+        name: "Alice".into(),
+    });
+
+    let response = client.send(request).await.unwrap();
+
+    println!("RESPONSE={:?}", response);
+}
+
+async fn server_streaming_hello<T>(client: &mut GreeterClient<T>, num: usize)
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody> + Send + 'static,
+    T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
+{
+    let stream = client
+        .send_stream(HelloRequest { name: "bob".into() })
+        .await
+        .unwrap()
+        .into_inner();
+
+    // stream is infinite - take just 5 elements and then disconnect
+    let mut stream = stream.take(num);
+    while let Some(item) = stream.next().await {
+        dbg!(&item);
+        println!("\treceived: {}", item.unwrap().message);
+    }
+    // stream is droped here and the disconnect info is send to server
+}
+
+async fn client_streaming_hello<T>(client: &mut GreeterClient<T>, num: usize)
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody> + Send + 'static,
+    T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
+{
+    let in_stream = hello_requests_iter().take(num);
+
+    let response = client.receive_stream(in_stream).await.unwrap();
+
+    println!("RESPONSE={:?}", response);
+}
+
+async fn bidirectional_streaming_echo<T>(client: &mut GreeterClient<T>, num: usize)
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody> + Send + 'static,
+    T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
+{
+    let in_stream = hello_requests_iter().take(num);
+
+    let response = client.bidirectional(in_stream).await.unwrap();
+
+    let mut resp_stream = response.into_inner();
+
+    while let Some(received) = resp_stream.next().await {
+        let received = received.unwrap();
+        println!("\treceived message: `{}`", received.message);
+    }
+}
+
+async fn bidirectional_streaming_echo_throttle<T>(client: &mut GreeterClient<T>, dur: Duration)
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody> + Send + 'static,
+    T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
+{
+    let in_stream = hello_requests_iter().throttle(dur);
+
+    let response = client.bidirectional(in_stream).await.unwrap();
+
+    let mut resp_stream = response.into_inner();
+
+    while let Some(received) = resp_stream.next().await {
+        let received = received.unwrap();
+        println!("\treceived message: `{}`", received.message);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let data_dir = std::path::PathBuf::from(std::env!("CARGO_MANIFEST_DIR"));
+    let data_dir = std::env::current_dir()?;
+    let fd = std::fs::File::open(data_dir.join("tls/ca.pem"))?;
+
+    let mut roots = RootCertStore::empty();
+
+    let mut buf = std::io::BufReader::new(&fd);
+    let certs = rustls_pemfile::certs(&mut buf)?;
+    roots.add_parsable_certificates(&certs);
+
+    let tls = ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(roots)
+        .with_no_client_auth();
+
+    let mut http = HttpConnector::new();
+    http.enforce_http(false);
+
+    let connector = tower::ServiceBuilder::new()
+        .layer_fn(move |s| {
+            let tls = tls.clone();
+
+            hyper_rustls::HttpsConnectorBuilder::new()
+                .with_tls_config(tls)
+                .https_or_http()
+                .enable_http2()
+                .wrap_connector(s)
+        })
+        .map_request(|_: Uri| Uri::from_static("https://127.0.0.1:50051"))
+        .service(http);
+    // .boxed_clone();
+
+    let client = hyper::Client::builder().build(connector);
+
+    // Using `with_origin` will let the codegenerated client set the `scheme` and
+    // `authority` from the porvided `Uri`.
+    let uri = Uri::from_static("https://example.com");
+
+    let mut client = GreeterClient::with_origin(client, uri);
+    let request = tonic::Request::new(HelloRequest {
+        name: "Alice".into(),
+    });
+
+    let response = client.send(request).await.unwrap();
+
+    println!("RESPONSE={:?}", response);
+    say_hello(&mut client).await;
+
+    // server_streaming_hello(&mut client, 5).await;
+
+    client_streaming_hello(&mut client, 5).await;
+
+    bidirectional_streaming_echo(&mut client, 5).await;
+
+    // bidirectional_streaming_echo_throttle(&mut client, Duration::from_millis(200)).await;
+
+    Ok(())
+}
---
+++ wasix-grpc/src/server.rs
@@ -0,0 +1,250 @@
+use hyper::server::conn::Http;
+use tokio_rustls::rustls::{OwnedTrustAnchor, RootCertStore, ServerConfig};
+use tonic::{transport::Server, Request, Response, Status};
+
+use hello_world::greeter_server::Greeter;
+use hello_world::{HelloReply, HelloRequest};
+
+use tokio::sync::mpsc;
+use tokio_stream::{wrappers::ReceiverStream, Stream, StreamExt};
+
+use std::error::Error;
+use std::io::ErrorKind;
+use std::pin::Pin;
+use std::time::Duration;
+
+use std::sync::Arc;
+use tokio::net::TcpListener;
+use tokio_rustls::{
+    rustls::{Certificate, PrivateKey},
+    TlsAcceptor,
+};
+use tower_http::ServiceBuilderExt;
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+}
+
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<HelloReply, Status>> + Send>>;
+
+fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
+    let mut err: &(dyn Error + 'static) = err_status;
+
+    loop {
+        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
+            return Some(io_err);
+        }
+
+        // h2::Error do not expose std::io::Error with `source()`
+        // https://github.com/hyperium/h2/pull/462
+        // if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
+        // if let Some(io_err) = h2_err.get_io() {
+        // return Some(io_err);
+        // }
+        // }
+
+        err = match err.source() {
+            Some(err) => err,
+            None => return None,
+        };
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn send(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+        Ok(Response::new(HelloReply {
+            message: format!("hello {}", request.get_ref().name),
+        }))
+    }
+
+    type SendStreamStream = ResponseStream;
+    async fn send_stream(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<Self::SendStreamStream>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let repeat = std::iter::repeat(HelloReply {
+            message: "hello".to_string(),
+        });
+
+        let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
+        let (tx, rx) = mpsc::channel(128);
+
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {
+                        // item (server response) was queued to be send to client
+                    }
+                    Err(_item) => {
+                        // output_stream was build from rx and both are dropped
+                        break;
+                    }
+                }
+            }
+            println!("\tclient disconnected");
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::SendStreamStream
+        ))
+    }
+
+    async fn receive_stream(
+        &self,
+        request: Request<tonic::Streaming<HelloRequest>>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let mut stream = request.into_inner();
+        let mut message = String::from("");
+        while let Some(req) = stream.message().await? {
+            message.push_str(&format!("Hello {}\n", req.name))
+        }
+        Ok(Response::new(HelloReply { message }))
+    }
+
+    type BidirectionalStream = ResponseStream;
+    async fn bidirectional(
+        &self,
+        request: Request<tonic::Streaming<HelloRequest>>,
+    ) -> Result<Response<Self::BidirectionalStream>, Status> {
+        let mut in_stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(128);
+
+        // this spawn here is required if you want to handle connection error.
+        // If we just map `in_stream` and write it back as `out_stream` the `out_stream`
+        // will be drooped when connection error occurs and error will never be propagated
+        // to mapped version of `in_stream`.
+        tokio::spawn(async move {
+            while let Some(result) = in_stream.next().await {
+                match result {
+                    Ok(req) => tx
+                        .send(Ok(HelloReply {
+                            message: format!("hello {}", req.name),
+                        }))
+                        .await
+                        .expect("working rx"),
+                    Err(err) => {
+                        if let Some(io_err) = match_for_io_error(&err) {
+                            if io_err.kind() == ErrorKind::BrokenPipe {
+                                // here you can handle special case when client
+                                // disconnected in unexpected way
+                                eprintln!("\tclient disconnected: broken pipe");
+                                break;
+                            }
+                        }
+
+                        match tx.send(Err(err)).await {
+                            Ok(_) => (),
+                            Err(_err) => break, // response was dropped
+                        }
+                    }
+                }
+            }
+            println!("\tstream ended");
+        });
+
+        let out_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(
+            Box::pin(out_stream) as Self::BidirectionalStream
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR")]);
+    // get the current directory
+    let data_dir = std::env::current_dir()?;
+    let certs = {
+        let fd = std::fs::File::open(data_dir.join("tls/server.pem"))?;
+        let mut buf = std::io::BufReader::new(&fd);
+        rustls_pemfile::certs(&mut buf)?
+            .into_iter()
+            .map(Certificate)
+            .collect()
+    };
+    let key = {
+        let fd = std::fs::File::open(data_dir.join("tls/server.key"))?;
+        let mut buf = std::io::BufReader::new(&fd);
+        rustls_pemfile::pkcs8_private_keys(&mut buf)?
+            .into_iter()
+            .map(PrivateKey)
+            .next()
+            .unwrap()
+
+        // let key = std::fs::read(data_dir.join("tls/server.key"))?;
+        // PrivateKey(key)
+    };
+
+    let mut tls = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)?;
+    tls.alpn_protocols = vec![b"h2".to_vec()];
+
+    let server = MyGreeter::default();
+
+    let svc = Server::builder()
+        .add_service(hello_world::greeter_server::GreeterServer::new(server))
+        .into_service();
+
+    let mut http = Http::new();
+    http.http2_only(true);
+
+    let listener = TcpListener::bind("127.0.0.1:50051").await?;
+    let tls_acceptor = TlsAcceptor::from(Arc::new(tls));
+
+    loop {
+        let (conn, addr) = match listener.accept().await {
+            Ok(incoming) => incoming,
+            Err(e) => {
+                eprintln!("Error accepting connection: {}", e);
+                continue;
+            }
+        };
+
+        let http = http.clone();
+        let tls_acceptor = tls_acceptor.clone();
+        let svc = svc.clone();
+
+        tokio::spawn(async move {
+            let mut certificates = Vec::new();
+
+            let conn = tls_acceptor
+                .accept_with(conn, |info| {
+                    if let Some(certs) = info.peer_certificates() {
+                        for cert in certs {
+                            certificates.push(cert.clone());
+                        }
+                    }
+                })
+                .await
+                .unwrap();
+
+            let svc = tower::ServiceBuilder::new()
+                .add_extension(Arc::new(ConnInfo { addr, certificates }))
+                .service(svc);
+
+            http.serve_connection(conn, svc)
+                .await
+                .map_err(|e| {
+                    eprintln!("Error serving connection: {}", e);
+                    e
+                })
+                .unwrap();
+        });
+    }
+}
+
+#[derive(Debug)]
+struct ConnInfo {
+    addr: std::net::SocketAddr,
+    certificates: Vec<Certificate>,
+}
